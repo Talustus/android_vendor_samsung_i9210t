719534d16ab82cbc0865320048f468a3933775dc
Talustus <talustus@dream-irc.de>
2013-01-31 23:49:09 +0100

    Testing: Add blobs for FM Radio
    Change-Id: Ib8cdc9a804322e33d0d0d9bd8fed58910d064613

--- i9210t-vendor-blobs.mk
+++ i9210t-vendor-blobs.mk
@@ -112,4 +112,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/i9210t/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
 	vendor/samsung/i9210t/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
 	vendor/samsung/i9210t/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-	vendor/samsung/i9210t/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw
+	vendor/samsung/i9210t/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+	vendor/samsung/skyrocket/proprietary/lib/libfmradio.brcm-prop_rx.so:system/lib/libfmradio.brcm-prop_rx.so \
+	vendor/samsung/skyrocket/proprietary/lib/libfmradio.so:system/lib/libfmradio.so \
+	vendor/samsung/skyrocket/proprietary/bin/btld:system/bin/btld
