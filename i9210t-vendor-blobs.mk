11f7937709ea10393a56ab957a813587fe6be8fb
Talustus <talustus@dream-irc.de>
2014-08-14 12:48:46 +0800

    msm8660 devices: rename camera hw blob to make way for wrapper * wrapper is implemented in http://review.cyanogenmod.org/#/c/55132/
    Change-Id: Ie53324daef78496b199d474e0b2bbefc96d9b0f8

--- i9210t-vendor-blobs.mk
+++ i9210t-vendor-blobs.mk
@@ -80,7 +80,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/i9210t/proprietary/lib/librpc.so:system/lib/librpc.so \
 	vendor/samsung/i9210t/proprietary/lib/hw/sensors.MSM8660_SURF.so:system/lib/hw/sensors.MSM8660_SURF.so \
 	vendor/samsung/i9210t/proprietary/lib/libakm.so:system/lib/libakm.so \
-	vendor/samsung/i9210t/proprietary/lib/hw/camera.msm8660.so:system/lib/hw/camera.msm8660.so \
+	vendor/samsung/i9210t/proprietary/lib/hw/camera.vendor.msm8660.so:system/lib/hw/camera.vendor.msm8660.so \
 	vendor/samsung/i9210t/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
 	vendor/samsung/i9210t/proprietary/lib/libgemini.so:system/lib/libgemini.so \
 	vendor/samsung/i9210t/proprietary/lib/libs3cjpeg.so:system/lib/libs3cjpeg.so \
